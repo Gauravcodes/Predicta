772d94d12a9ea5f09cb626b8b512fb5fe5086130
GP <parasha.gaurav@gmail.com>
2017-02-15 20:56:47 +1100

    updated

--- webScrapper/testing.py
+++ webScrapper/testing.py
@@ -1,22 +1,18 @@
 import urllib.request
 from bs4 import BeautifulSoup
 
-sports = ["cricket","soccer","boxing","snooker","basketball"]
+urls = ["https://www.sportsbet.com.au/account?HeaderAccountNo"]
 
-#urls = ["http://www.sportsbet.com.au/betting/soccer?MegaNav", "http://www.sportsbet.com.au/betting/cricket?MegaNav",
- #       "http://www.sportsbet.com.au/betting/snooker?MegaNav", "http://www.sportsbet.com.au/betting/basketball?MegaNav"]
+for url in urls:
 
-for sport in sports:
-
-    url = "http://www.sportsbet.com.au/betting/"+sport+"?MegaNav"
-    print(url)
     htmlFile = urllib.request.urlopen(url)
     htmlText = htmlFile.read()
     soup = BeautifulSoup(htmlText, "lxml")
     links = soup.find_all("div", {"class": lambda l: l and l.startswith('price-link')})
-
-    for link in links:
+    print(htmlText)
+    """for link in links:
         team = link("span", {"class": "team-name ib"})[0].string
         rate = link("span", {"class": lambda l1: l1 and l1.startswith('price-val')})[0].string
         print(team+rate)
     print("Next Sports")
+"""
